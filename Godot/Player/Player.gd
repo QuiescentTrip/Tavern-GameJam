323f63e24f8e08c5183456978fba238448a5c699
QuiescentTrip <bluevs10@hotmail.no>
2023-09-21 18:52:32 +0200

    sprites

--- Godot/Player/Player.gd
+++ Godot/Player/Player.gd
@@ -6,10 +6,11 @@ var center = Vector2()
 var damping = 12.0
 var team = 0
 var friction = 0.18
-var speed = 400
+var speed = 500
 var bulletspeed = 4000
 var empty = true
 
+@onready var AnimatedSprite = $AnimatedSprite2D
 @onready var end = $Marker2D
 #var player = preload("res://Player/Player.tscn")
 var bullet = preload("res://bullet.tscn")
@@ -40,6 +41,7 @@ func _physics_process(_delta: float) -> void:
 
 func fire():
 	$Shot.play()
+	AnimatedSprite.frame = 0
 	var bullet_instance = bullet.instantiate()
 	var direction = (end.global_position - global_position).normalized()
 	GlobalSignals.bullet_fired.emit(bullet_instance, end.global_position, direction, 0)
@@ -50,7 +52,9 @@ func onkill():
 	if !empty:
 		queue_free()
 	else:
+		AnimatedSprite.frame = 1
 		empty = false
+		
 
 func bulletkill():
 	queue_free()
